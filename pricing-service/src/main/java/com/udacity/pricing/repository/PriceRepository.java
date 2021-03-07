f5de5b5f2f727865c592807ec2bd199c9140541f
Ming Ho <28768492+super-ming@users.noreply.github.com>
2021-03-07 14:00:14 -0800

    Add price repository

--- pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.udacity.pricing.repository;
-
-import com.udacity.pricing.microservices.entity.Price;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PriceRepository extends CrudRepository<Price, Long> {
-}
