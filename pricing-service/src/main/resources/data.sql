4eebfa3144e1673d7ecb030e95c0b9b5b50d1110
Ming Ho <28768492+super-ming@users.noreply.github.com>
2021-03-07 16:05:30 -0800

    Add test

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO price (currency, price, vehicle_id) VALUES ('USD', 1.00, 1);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('EUR', 2.00, 3);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('CAD', 3.00, 2);
--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,37 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.repository.PriceRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+	@Autowired
+	PriceRepository priceRepository;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	public void contextLoads() {
+	public void testGetAllPrices() throws Exception {
+		mockMvc.perform(get("/prices"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$._embedded").exists())
+				.andExpect(jsonPath("$._embedded.prices").exists())
+				.andExpect(jsonPath("$._embedded.prices", hasSize(3)));
 	}
 
 }
